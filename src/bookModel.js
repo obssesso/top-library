a6180a61109390848984e7a8f2bc603bfa1dc018
obssesso <122223376+obssesso@users.noreply.github.com>
2023-07-19 09:55:45 +0200

    feat: set up DB and CRUD

--- src/bookModel.js
+++ src/bookModel.js
@@ -1,15 +1,44 @@
 import { openDB, deleteDB, wrap, unwrap } from "idb";
 
-export default class bookModel extends EventTarget {
-  constructor() {
-    super();
+export default function bookModelFactory() {
+  let db;
+  let books;
+
+  let bookModel = Object.create(EventTarget.prototype);
+
+  async function initModel() {
+    const db = await getDatabase();
+    const transaction = db.transaction(["books"], "readonly");
+    const store = transaction.objectStore("books");
+    books = await store.getAll();
   }
 
-  upgrade(db) {
-    db.createObjectStore("test", { keyPath: "id", autoIncrement: true });
+  async function getDatabase() {
+    return await openDB("bookStorage", 1, { upgrade });
   }
 
-  async openDB() {
-    return await openDB("testDB", 1, { upgrade: (db) => this.upgrade(db) });
+  function upgrade(db) {
+    if (!db.objectStoreNames.contains("books")) {
+      db.createObjectStore("books", { keyPath: "id" });
+    }
   }
+
+  async function add(book) {
+    books.push(book);
+    update();
+  }
+
+  async function update() {
+    const transaction = db.transaction("books", "readwrite");
+    const store = transaction.objectStore("books");
+    store.put(books);
+    try {
+      bookModel.dispatchEvent(new CustomEvent("update"));
+    } catch (error) {}
+  }
+
+  bookModel.add = add;
+  initModel();
+
+  return { bookModel };
 }
