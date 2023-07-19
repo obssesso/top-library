36d47abbcd5c38e59e9da65ef11d323a4de6e05d
obssesso <122223376+obssesso@users.noreply.github.com>
2023-07-19 21:03:18 +0200

    feat: setup static data pojo for testing purposes

--- src/bookModel.js
+++ src/bookModel.js
@@ -1,29 +1,62 @@
 import { openDB, deleteDB, wrap, unwrap } from "idb";
+import enumFactory from "./helpers.js";
 
-export default function bookModelFactory() {
+export default async function bookModelFactory() {
   let db;
   let books;
+  const bookStatus = enumFactory(["Read", "Want to Read", "Currently Reading"]);
 
-  let bookModel = Object.create(EventTarget.prototype);
+/*   let bookModel = Object.create(EventTarget.prototype); */
+
+  const bookModel = new EventTarget();
+
+  bookModel.add = add;
+  bookModel.getBooks = getBooks;
+  await initModel();
+
+  return bookModel;
 
   async function initModel() {
-    const db = await getDatabase();
+    books = [
+      {
+        id: 1,
+        title: "The Great Gatsby",
+        author: "Scott F. Fitzgerald",
+        rating: 5,
+        status: bookStatus.currentlyReading,
+      },
+      {
+        id: 2,
+        title: "1984",
+        author: "George Orwell",
+        rating: 5,
+        status: bookStatus.read,
+      },
+      {
+        id: 3,
+        title: "Jane Eyre",
+        author: "George Orwell",
+        rating: 4,
+        status: bookStatus.wantToRead,
+      },
+    ];
+    db = await getDatabase();
     const transaction = db.transaction(["books"], "readonly");
     const store = transaction.objectStore("books");
-    books = await store.getAll();
+    /*     books = await store.getAll(); */
   }
 
-  async function getDatabase() {
-    return await openDB("bookStorage", 1, { upgrade });
+  function getDatabase() {
+    return openDB("bookStorage", 1, { upgrade });
   }
 
   function upgrade(db) {
     if (!db.objectStoreNames.contains("books")) {
-      db.createObjectStore("books", { keyPath: "id" });
+      db.createObjectStore("books", { keyPath: "id", autoIncrement: true });
     }
   }
 
-  async function add(book) {
+  function add(book) {
     books.push(book);
     update();
   }
@@ -31,14 +64,18 @@ export default function bookModelFactory() {
   async function update() {
     const transaction = db.transaction("books", "readwrite");
     const store = transaction.objectStore("books");
-    store.put(books);
+
+    const bookPromises = books.map((book) => store.put(book));
+
     try {
+      await Promise.all(bookPromises);
       bookModel.dispatchEvent(new CustomEvent("update"));
-    } catch (error) {}
+    } catch (error) {
+      bookModel.dispatchEvent(new CustomEvent("updateFailed"));
+    }
   }
 
-  bookModel.add = add;
-  initModel();
-
-  return { bookModel };
+  function getBooks() {
+    return books;
+  }
 }
