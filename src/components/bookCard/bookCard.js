1aa8d1e1d7f70525db8588b1e9b1d9595f1b3cd5
obssesso <122223376+obssesso@users.noreply.github.com>
2023-07-20 22:55:40 +0200

    fix: fix bug where svg star became unclickable when fill of star path was colorized therefore blocking the hit area of the svg.
    solution: pointer-events of path in star svg set to none

--- src/components/bookCard/bookCard.js
+++ src/components/bookCard/bookCard.js
@@ -101,31 +101,31 @@ export default function bookCardFactory() {
                                     xmlns="http://www.w3.org/2000/svg">
                                     <path
                                         d="M14.65 8.93274L12.4852 4.30901C12.2923 3.89699 11.7077 3.897 11.5148 4.30902L9.35002 8.93274L4.45559 9.68243C4.02435 9.74848 3.84827 10.2758 4.15292 10.5888L7.71225 14.2461L6.87774 19.3749C6.80571 19.8176 7.27445 20.1487 7.66601 19.9317L12 17.5299L16.334 19.9317C16.7256 20.1487 17.1943 19.8176 17.1223 19.3749L16.2878 14.2461L19.8471 10.5888C20.1517 10.2758 19.9756 9.74848 19.5444 9.68243L14.65 8.93274Z"
-                                        stroke="#464455" stroke-linecap="round" stroke-linejoin="round" />
+                                        stroke="#464455" stroke-linecap="round" stroke-linejoin="round" pointer-events="none"/>
                                 </svg>
                                 <svg data-star-number=4 width="17px" height="17px" viewBox="0 0 24 24" fill="none"
                                     xmlns="http://www.w3.org/2000/svg">
                                     <path
                                         d="M14.65 8.93274L12.4852 4.30901C12.2923 3.89699 11.7077 3.897 11.5148 4.30902L9.35002 8.93274L4.45559 9.68243C4.02435 9.74848 3.84827 10.2758 4.15292 10.5888L7.71225 14.2461L6.87774 19.3749C6.80571 19.8176 7.27445 20.1487 7.66601 19.9317L12 17.5299L16.334 19.9317C16.7256 20.1487 17.1943 19.8176 17.1223 19.3749L16.2878 14.2461L19.8471 10.5888C20.1517 10.2758 19.9756 9.74848 19.5444 9.68243L14.65 8.93274Z"
-                                        stroke="#464455" stroke-linecap="round" stroke-linejoin="round" />
+                                        stroke="#464455" stroke-linecap="round" stroke-linejoin="round" pointer-events="none"/>
                                 </svg>
                                 <svg data-star-number=3 width="17px" height="17px" viewBox="0 0 24 24" fill="none"
                                     xmlns="http://www.w3.org/2000/svg">
                                     <path
                                         d="M14.65 8.93274L12.4852 4.30901C12.2923 3.89699 11.7077 3.897 11.5148 4.30902L9.35002 8.93274L4.45559 9.68243C4.02435 9.74848 3.84827 10.2758 4.15292 10.5888L7.71225 14.2461L6.87774 19.3749C6.80571 19.8176 7.27445 20.1487 7.66601 19.9317L12 17.5299L16.334 19.9317C16.7256 20.1487 17.1943 19.8176 17.1223 19.3749L16.2878 14.2461L19.8471 10.5888C20.1517 10.2758 19.9756 9.74848 19.5444 9.68243L14.65 8.93274Z"
-                                        stroke="#464455" stroke-linecap="round" stroke-linejoin="round" />
+                                        stroke="#464455" stroke-linecap="round" stroke-linejoin="round" pointer-events="none"/>
                                 </svg>
                                 <svg data-star-number=2 width="17px" height="17px" viewBox="0 0 24 24" fill="none"
                                     xmlns="http://www.w3.org/2000/svg">
                                     <path
                                         d="M14.65 8.93274L12.4852 4.30901C12.2923 3.89699 11.7077 3.897 11.5148 4.30902L9.35002 8.93274L4.45559 9.68243C4.02435 9.74848 3.84827 10.2758 4.15292 10.5888L7.71225 14.2461L6.87774 19.3749C6.80571 19.8176 7.27445 20.1487 7.66601 19.9317L12 17.5299L16.334 19.9317C16.7256 20.1487 17.1943 19.8176 17.1223 19.3749L16.2878 14.2461L19.8471 10.5888C20.1517 10.2758 19.9756 9.74848 19.5444 9.68243L14.65 8.93274Z"
-                                        stroke="#464455" stroke-linecap="round" stroke-linejoin="round" />
+                                        stroke="#464455" stroke-linecap="round" stroke-linejoin="round" pointer-events="none"/>
                                 </svg>
                                 <svg data-star-number=1 width="17px" height="17px" viewBox="0 0 24 24" fill="none"
                                     xmlns="http://www.w3.org/2000/svg">
                                     <path
                                         d="M14.65 8.93274L12.4852 4.30901C12.2923 3.89699 11.7077 3.897 11.5148 4.30902L9.35002 8.93274L4.45559 9.68243C4.02435 9.74848 3.84827 10.2758 4.15292 10.5888L7.71225 14.2461L6.87774 19.3749C6.80571 19.8176 7.27445 20.1487 7.66601 19.9317L12 17.5299L16.334 19.9317C16.7256 20.1487 17.1943 19.8176 17.1223 19.3749L16.2878 14.2461L19.8471 10.5888C20.1517 10.2758 19.9756 9.74848 19.5444 9.68243L14.65 8.93274Z"
-                                        stroke="#464455" stroke-linecap="round" stroke-linejoin="round" />
+                                        stroke="#464455" stroke-linecap="round" stroke-linejoin="round" pointer-events="none"/>
                                 </svg>
                             </div>
                         </div>
